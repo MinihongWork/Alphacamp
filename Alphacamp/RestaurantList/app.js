21eb64f4d51f454248209b9d86370a16c3192d27
MinihongWork <henryabout15@hotmail.com>
2022-03-13 01:12:41 +0800

    modify jsonpath to dbpath

--- Alphacamp/RestaurantList/app.js
+++ Alphacamp/RestaurantList/app.js
@@ -1,7 +1,9 @@
 // require packages used in the project
+
 const express = require('express')
 const app = express()
 const port = 3000
+const restaurant = require('./models/restaurant.js')
 app.use(express.static('public'))
 
 // require express-handlebars here
@@ -11,22 +13,49 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//mongodb connect
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/Restaurant-List', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 app.get('/',(req,res) => {
-  res.render('index',{restaurants : restaurant.results})
+  restaurant.find()
+  .lean()
+  .then(restaurant => res.render('index',{restaurants : restaurant}))
+  .catch(error => console.log(error))
+  
 })
 
 app.get('/restaurants/:restaurant_id',(req,res) => {
-    const RestaurantContent = restaurant.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)    
+  restaurant.find()
+  .lean()
+  .then( restaurant =>{
+    const RestaurantContent = restaurant.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)    
     res.render('content',{restaurants : RestaurantContent})
+    })
+  .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
+  restaurant.find()
+  .lean()
+  .then( restaurant =>{
     const keyword = req.query.keyword
     const restaurants = restaurant.results.filter(restaurant => {
       return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
     })
     res.render('index', { restaurants: restaurants, keyword: keyword })
   })
+  .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
